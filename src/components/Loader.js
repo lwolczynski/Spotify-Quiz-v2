7626ae846ca330e3c1929901042aa24dae204bd8
unknown <lwolczynski7@gmail.com>
2020-12-17 15:28:14 -0600

    Add bootstrap spinner to the Loader component

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Spinner } from 'react-bootstrap'
 
 const Loader = () => {
     return (
         <div className="row flex-fill justify-content-center align-items-center">
-            Signing in...
+            <Spinner animation="border" role="status">
+                <span className="sr-only">Loading...</span>
+            </Spinner>
         </div>
     )
 }
