e86a3615ec4e2f08eb3c3c2386fba8b6f5817b8e
unknown <lwolczynski7@gmail.com>
2020-12-21 14:28:11 -0600

    Add game answer buttons colored and disabled after click

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -8,9 +8,9 @@ import Button from 'react-bootstrap/Button'
 const Game = () => {
     const [tracks, setTracks] = useState(null)
     const [trackNumber, setTrackNumber] = useState(0)
-    const [score, setScore] = useState(0)
+    const [score, setScore] = useState({'correct': 0, 'wrong': 0, 'left': null})
     const [started, setStarted] = useState(false)
-    const [search, setSearch ] = useState('')
+    const [search, setSearch] = useState('')
 
     const location = useLocation();
 
@@ -18,14 +18,39 @@ const Game = () => {
         const execute = async () => {
             const fetchedTracks = await getAllTracks(location.state.playlistUrl, location.state.playlistType);
             setTracks(fetchedTracks.items);
+            setScore(prevState => ({
+                ...prevState,
+                ['left']: fetchedTracks.items.length
+            }))
         }
         execute()
     }, [])
 
+    const answer = ({ item }) => {
+        tracks[trackNumber].answered = true
+        if (tracks[trackNumber].id === item.id) {
+            setScore(prevState => ({
+                ...prevState,
+                ['correct']: score['correct']+1,
+                ['left']: score['left']-1
+            }))
+            tracks[trackNumber].guessed = true
+        } else {
+            setScore(prevState => ({
+                ...prevState,
+                ['wrong']: score['wrong']+1,
+                ['left']: score['left']-1
+            }))
+            tracks[trackNumber].guessed = false
+        }
+        setSearch('')
+        setTrackNumber(trackNumber+1)
+    }
+    
     const renderTracks = () => {
         const searchRegex = new RegExp(search, "i");
         return tracks.reduce((result, item) => {
-            return (searchRegex.test(item.name) || searchRegex.test(item.artists[0].name)) ? [...result, <Button variant="outline-dark" onClick={() => setTrackNumber(trackNumber+1)}>{item.name} by {item.artists[0].name}</Button>] : result
+            return (searchRegex.test(item.name) || searchRegex.test(item.artists[0].name)) ? [...result, <Button variant={item.answered ? (item.guessed ? "success" : "danger") : "outline-dark"} disabled={item.answered || !started ? true : false} onClick={() => answer({item})}>{item.name} by {item.artists[0].name}</Button>] : result
         }, [])
     }
 
@@ -38,7 +63,7 @@ const Game = () => {
                     {started ? <Track track={tracks[trackNumber]} /> : <Button onClick={() => setStarted(true)}>Start</Button>}
                 </div>
                 <div className="col-6">
-                    <h3>Your score: {score}</h3>
+                    <h3>Your score: {score.correct}/{tracks.length}</h3>
                 </div>
             </div>
             <div className="row">
--- src/components/Game/Track.js
+++ src/components/Game/Track.js
@@ -3,7 +3,6 @@ import React, { useEffect } from 'react'
 const Track = ({ track }) => {
 
     useEffect(() => {
-        console.log(track)
         const audio = new Audio(track.preview_url);
         audio.loop = true;
         audio.play();
