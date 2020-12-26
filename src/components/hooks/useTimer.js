530b0469653fd81a8be811b3646fb2546cff0ea3
unknown <lwolczynski7@gmail.com>
2020-12-26 09:02:35 -0600

    Bug fix to timer running from 0 after an hour

--- src/components/hooks/useTimer.js
+++ src/components/hooks/useTimer.js
@@ -30,7 +30,7 @@ const useTimer = () => {
     }
 
     const printTimer = () => {
-        const getSeconds = `0${(timer % 60)}`.slice(-2)
+        const getSeconds = `0${timer}`.slice(-2)
         const minutes = `${Math.floor(timer / 60)}`
         const getMinutes = `0${minutes % 60}`.slice(-2)
     
