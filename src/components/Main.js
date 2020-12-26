b70019f5c7240d79471fa901720d3001adc98638
unknown <lwolczynski7@gmail.com>
2020-12-26 15:03:46 -0600

    Add text to landing page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import CircleType from 'circletype'
 
 const Main = () => {
 
+    const circleBottom = useRef();
+
+    useEffect(() => {
+        new CircleType(circleBottom.current).radius(230).dir(-1)
+    }, []);
+
     return (
         <div className="row flex-fill justify-content-center align-items-center">
-            <img src="logo512.png" width="450" height="450" className="d-inline-block align-top" alt="" />
+            <div id="landing-wrapper" className="d-inline-block align-top">
+                <img src="logo512.png" id="landing-logo" alt="" />
+                <h3 ref={circleBottom} id="slogan">How well you know your music?</h3>
+            </div>
         </div>
     )
 }
