d9bc0f555f817a47abd58c6d33cf1ae3203eaf14
unknown <lwolczynski7@gmail.com>
2020-12-26 20:57:40 -0600

    Change to Track component to let music play on iPhone (autoplay issue)

--- src/components/Game/Track.js
+++ src/components/Game/Track.js
@@ -1,28 +1,23 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import Image from 'react-bootstrap/Image'
 
 const Track = ({ track, paused }) => {
-    const [audio, setAudio] = useState(null)
+    
+    const myAudio = useRef()
 
     useEffect(() => {
-        const audio = new Audio(track.preview_url)
-        setAudio(audio)
-        audio.loop = true
-        audio.play()
-        
-        return () => {
-            audio.pause()
-        }
+        myAudio.current.play()  
     }, [track])
 
     useEffect(() => {
-        if (audio) {
-            paused ? audio.pause() : audio.play()
-        }
+        paused ? myAudio.current.pause() : myAudio.current.play()
     }, [paused]) // eslint-disable-line react-hooks/exhaustive-deps
 
     return (
-        <Image className="game-album" src={track.album.images[0].url} />
+        <>
+            <Image className="game-album" src={track.album.images[0].url} />
+            <audio src={track.preview_url} ref={myAudio} type='audio' style={{ display: 'none' }} loop />
+        </>
     )
 }
 
