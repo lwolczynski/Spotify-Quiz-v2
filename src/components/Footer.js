a93fb314cc144abfd6e34c4493e84d8dd1f93b42
unknown <lwolczynski7@gmail.com>
2020-12-27 11:32:19 -0600

    Change of design to address issue with rendering on iPhones

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer>
+        <footer class="container">
             <small className="span-footer">
                 © 2020 created by <a href="https://github.com/lwolczynski">lwolczynski</a>
             </small>
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import Navbar from 'react-bootstrap/Navbar'
 const Header = ({ children }) => {
 
     return (
-        <Navbar className="py-0" >
+        <Navbar className="py-0 container" >
             <Navbar.Brand className="mr-auto">
                 <Link to="/" className="navbar-brand">
                     <img src="/favicon.ico" width="30" heigth="30" className="d-inline-block align-top" alt="" />
