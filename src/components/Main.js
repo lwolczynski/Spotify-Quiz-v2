65fbfeaa782dc8db678b2265854f12bfa718ef3f
unknown <lwolczynski7@gmail.com>
2020-12-27 19:50:01 -0600

    Fix to styling on landing page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react'
 import CircleType from 'circletype'
+import Container from 'react-bootstrap/Container'
 
 const Main = () => {
 
@@ -10,12 +11,12 @@ const Main = () => {
     }, []);
 
     return (
-        <div className="row flex-fill justify-content-center align-items-center">
+        <Container className="main d-flex justify-content-center align-items-center">
             <div id="landing-wrapper" className="d-inline-block align-top">
                 <img src="logo512.png" id="landing-logo" alt="" />
                 <h3 ref={circleBottom} id="slogan">How well do you know your music?</h3>
             </div>
-        </div>
+        </Container>
     )
 }
 
