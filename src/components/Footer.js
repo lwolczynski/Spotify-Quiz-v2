f314ef2e3c7ee59e23c5d7bd04465d11107b83d9
unknown <lwolczynski7@gmail.com>
2021-01-18 11:43:44 -0600

    Change to portfolio link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="container">
             <small className="span-footer">
-                © 2020 created by <a href="https://lwolczynski.com">lwolczynski</a>
+                © 2020 created by <a href="https://lwolczynski.com" target="_blank">lwolczynski</a>
             </small>
         </footer>
     )
