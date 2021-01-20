1f78bb71662c5ffc1c8fc5d04a33e399eb90fa3c
unknown <lwolczynski7@gmail.com>
2021-01-19 19:22:31 -0600

    Fix to external link vulnerability by adding rel noopener

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="container">
             <small className="span-footer">
-                © 2020 created by <a href="https://lwolczynski.com" target="_blank">lwolczynski</a>
+                © 2020 created by <a href="https://lwolczynski.com" target="_blank" rel="noopener">lwolczynski</a>
             </small>
         </footer>
     )
